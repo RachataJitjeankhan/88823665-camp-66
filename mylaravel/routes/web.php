55dc24f48ef371715c0617cc901e8490a2d3a927
RachataJitjeankhan <tang1593572@gmail.com>
2025-01-28 18:53:49 +0700

    Update web.php

--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -3,17 +3,9 @@
 use App\Http\Controllers\MyController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/hello', function () {
-    return "<h1>Hello World</h1>";
-});
-
 Route::get('/mycontroller/{id?}', [MyController::class,'myfunction']);
 Route::post('/mycontroller/{id?}', [MyController::class,'myfunction']);
-
+    
 Route::get('/login',
 [App\Http\Controllers\LoginController::class, 'index']);
 
