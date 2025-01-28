7c4fc76784fd9d99cb352d94769ca86c93f7b20c
RachataJitjeankhan <tang1593572@gmail.com>
2025-01-28 18:59:56 +0700

    Update web.php

--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -13,15 +13,4 @@ Route::get('/hello', function () {
 
 Route::get('/mycontroller/{id?}', [MyController::class,'myfunction']);
 Route::post('/mycontroller/{id?}', [MyController::class,'myfunction']);
-    
-Route::get('/login',
-[App\Http\Controllers\LoginController::class, 'index']);
 
-Route::get('/home',
-[App\Http\Controllers\HomeController::class, 'index']);
-
-Route::get('/register',
-[App\Http\Controllers\RegisterController::class, 'index']);
-
-Route::get('/',
-[App\Http\Controllers\HomeController::class, 'index']);
