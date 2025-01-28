53bb15c5565b4e6cd07ac7d73e8b0ad89322763f
RachataJitjeankhan <tang1593572@gmail.com>
2025-01-28 18:08:43 +0700

    login

--- mylaravel/resources/views/myview.blade.php
+++ mylaravel/resources/views/myview.blade.php
@@ -1,59 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
+<h1> My View </h1>
+<?php echo $value_id; ?>
+{{ $value_id }} {{ $myinput }}
 
-<head>
-    <meta charset="UTF-8"> 
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <meta name="rachi" content="66160369">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Thai:wght@100..900&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="style.css">
-</head>
-
-<body>
-    <main>
-
-        <div class="container">
-        <div class="container d-flex justify-content-center align-items-center" style="min-height: 10vh;">
-    <h1 class="text-center">Workshop สูตรคูณ #LARAVEL</h1>
+<form action="{{ url('/mycontroller') }}" method="post">
+    @csrf
+    <input type="text" name="myinput">
+    <button type="submit">
+        submit
+    </button>
+</form>
+<?php for ($i = 1; $i <= 12; $i++) { ?>
+<div class=''>
+    <p class='m-0'>{{ $myinput }} x {{ $i }} = {{ $i * $myinput }}</p>
 </div>
-
-            <form action="{{url('/mycontroller')}}"
-                method="post">
-                @csrf
-                <div class="mb-3">
-                    <label for="typeNumber" class="form-label">Type a numbers to generate a multiplication table.</label>
-                    <input type="text" class="form-control" id="typeNumber" name="myinput">
-
-                </div>
-                <button type="submit" name="submit" class="btn btn-primary">Generate</button>
-            </form>
-
-            <div class='border mb-4'>
-                <div class=''>
-                    <h2 class=''>Table: {{$myinput}}</h2>
-                    <div class=''>
-                        <?php for ($i = 1; $i <= 12; $i++) { ?>
-
-                            <div class=''>
-                                <p class='m-0'>{{$myinput}} x {{$i}} = {{$i * $myinput}}</p>
-                            </div>
-                        <?php } ?>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </main>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"></script>
-
-</body>
-
-</html>
+<?php } ?>
--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\MyController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
